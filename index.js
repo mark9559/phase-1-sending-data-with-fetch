08307a1573fabcaf5c1855f59bc0ccf91151374d
Mark Mwangi <mwangimark57@gmail.com>
2023-10-03 22:29:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({ name: "Steve", email: "steve@steve.com" })
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(response => response.json())
+      .then(data => {
+        // Append the new ID to the DOM
+        const id = data.id;
+        const idElement = document.createElement("p");
+        idElement.textContent = id;
+        document.body.appendChild(idElement);
+      })
+      .catch(error => {
+        // Append error message to the DOM
+        const errorElement = document.createElement("p");
+        errorElement.textContent = error.message;
+        document.body.appendChild(errorElement);
+      });
+  }
+  
+  submitData("Mark Mwangi", "mwangimarkt@gmail")
+  
